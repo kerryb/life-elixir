57d0298414505c9b5e522e224180c5729d10564d
Kerry Buckley <kerryjbuckley@gmail.com>
2025-02-06 20:57:21 +0000

    Manhandle Life.Grid into one-line functions too

--- lib/life/grid.ex
+++ lib/life/grid.ex
@@ -1,23 +1,33 @@
 defmodule Life.Grid do
   def render(life) do
-    {{min_x, _}, {max_x, _}} = Enum.min_max_by(life.cells, &elem(&1, 0))
-    {{_, min_y}, {_, max_y}} = Enum.min_max_by(life.cells, &elem(&1, 1))
-    render_grid(life, min_x, max_x, min_y, max_y)
+    render_grid(life.cells, min_max_x(life.cells), min_max_y(life.cells))
   end
 
-  defp render_grid(life, min_x, max_x, min_y, max_y) do
-    Enum.map_join(min_y..max_y, "\n",  &render_row(life, &1, min_x, max_x)) <> "\n"
+  defp min_max_x(cells) do
+    cells |> Enum.min_max_by(&elem(&1, 0)) |> then(fn {{min, _}, {max, _}} -> {min, max} end)
   end
 
-  defp render_row(life, y, min_x, max_x) do
-    Enum.map_join(min_x..max_x, &render_coordinate(life, &1, y))
+  defp min_max_y(cells) do
+    cells |> Enum.min_max_by(&elem(&1, 1)) |> then(fn {{_, min}, {_, max}} -> {min, max} end)
   end
 
-  defp render_coordinate(life, x, y) do
-    if MapSet.member?(life.cells, {x, y}) do
-      "◼︎"
-    else
-      " "
-      end
+  defp render_grid(cells, {min_x, max_x}, {min_y, max_y}) do
+    Enum.map_join(min_y..max_y, "\n", &render_row(cells, &1, min_x, max_x)) <> "\n"
+  end
+
+  defp render_row(cells, y, min_x, max_x) do
+    Enum.map_join(min_x..max_x, &render_coordinate(cells, &1, y))
+  end
+
+  defp render_coordinate(cells, x, y) do
+    character_for(MapSet.member?(cells, {x, y}))
+  end
+
+  defp character_for(true) do
+    "◼︎"
+  end
+
+  defp character_for(false) do
+    " "
   end
 end
