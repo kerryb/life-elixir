192e95bdfc48bd2e80471e4fc0467b771f3a247b
Kerry Buckley <kerryjbuckley@gmail.com>
2025-02-06 13:15:13 +0000

    Slime zero neighbours case

--- lib/life.ex
+++ lib/life.ex
@@ -1,2 +1,12 @@
 defmodule Life do
+  @enforce_keys [:cells]
+  defstruct [:cells]
+
+  def new(cells) do
+    %__MODULE__{cells: MapSet.new(cells)}
+  end
+
+  def tick(_life) do
+    new([])
+  end
 end
--- test/life_test.exs
+++ test/life_test.exs
@@ -1,3 +1,10 @@
 defmodule LifeTest do
   use ExUnit.Case
+
+  describe "Life.tick/1" do
+    test "kills a cell with no neighbours" do
+      life = Life.new([{1, 1}])
+      assert MapSet.size(Life.tick(life).cells) == 0
+    end
+  end
 end
